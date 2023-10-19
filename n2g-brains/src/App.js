6790e03c46d6ac1710fdbae43714c6ccc238508d
NarekGhazaryanjs <narekghazaryanjs@gmail.com>
2023-10-19 21:29:21 +0400

    adding courses component with ui

--- n2g-brains/src/App.js
+++ n2g-brains/src/App.js
@@ -2,11 +2,13 @@ import { Fragment } from 'react';
 import './App.css';
 import { Route, Routes, Link } from "react-router-dom"
 import Contacts from './UI/pages/Contacts/Contacts';
+import Courses from './UI/pages/Courses/Courses';
 
 const App = () => {
   return (
     <Fragment>
-        <Contacts />
+        {/* <Contacts /> */}
+        <Courses />
     </Fragment>
   )
 }
--- n2g-brains/src/UI/pages/Courses/Courses.jsx
+++ n2g-brains/src/UI/pages/Courses/Courses.jsx
@@ -1,20 +1,46 @@
 import Block from "../../../Components/Block/Block"
 import List from "../../../Components/List/List"
+import Listitem from "../../../Components/Listitem/Listitem"
+import classes from '../../GlobalsCss/Global.module.scss'
+import Footer from "../../uiComponents/Footer/Footer"
+import Wrapper from "../../uiComponents/Wrapper/Wrapper"
 
 const Courses = ({
-  className,
+    className,
 }) => {
 
     const coursesArray = [
-        {name: 'Frontend', days: 2, level: "beginner", price: 35_000},
-        {name: 'Frontend', days: 3, level: "beginner", price: 50_000},
+        { name: 'Frontend', days: 2, level: "beginner", price: 35_000, duration: "8 months", id: 1 },
+        { name: 'Frontend', days: 3, level: "beginner", price: 50_000, duration: "6 months", id: 2 },
+        { name: 'SMM', days: 2, level: "beginner", price: 45_000, duration: "2 months", id: 3 },
+        { name: 'Graphic Design', days: 2, level: "beginner", price: 35_000, duration: "2 months", id: 4 },
+        { name: 'Backend (Node.js)', days: 3, level: "beginner", price: 45_000, duration: "4 months", id: 5 },
+        { name: 'Full Stack (React, Node.js / Express.js)', days: 3, level: "beginner", price: 55_000, duration: "8 months", id: 6 },
     ]
+
     return (
-        <Block>
-            <List>
-             
-            </List>
-        </Block>
+        <Wrapper>
+
+
+            <Block className={classes['courses-container']}>
+                <List className={classes['courses-container-list']}>
+                    {coursesArray.map(course => {
+                        return (
+                            <Listitem className={classes['courses-container-list-item']} key={course.id}>
+                                <span> name: {course.name} </span>
+                                <span> days: {course.days} </span>
+                                <span> level: {course.level} </span>
+                                <span> price: {course.price} </span>
+                                <span> duration: {course.duration} </span>
+                            </Listitem>
+                        )
+                    })}
+                </List>
+            </Block>
+
+            <Footer />
+
+        </Wrapper>
     )
 }
 
